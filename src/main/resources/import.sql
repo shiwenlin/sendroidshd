e408ffbc876202eccef296f90ecfb75626de8d73
edisonlao <350482257@qq.com>
2017-11-09 10:35:14 +0800

    ~new_case UI\add_user UI

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,8 +2,10 @@
 
 insert  into `sys_role`(`id`,`name`) values (1,'ROLE_ADMIN'),(2,'ROLE_USER');
 
+Create PROCEDURE proc()
+BEGIN
 
-insert  into `sys_user`(`id`,`create_time`,`email`,`password`,`phone`,`username`,`name`) values (1,'2017-11-06','1160431608@qq.com','admin','18211111111','admin','张三');
+insert  into `sys_user`(`id`,`create_time`,`email`,`password`,`phone`,`username`,`name`) values (1,'2017-11-06','1160431608@qq.com','123456','18211111111','admin1','张三');
 
 
 insert  into `sys_user_roles`(`sys_user_id`,`roles_id`) values (1,1);
